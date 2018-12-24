55cc9c425e8cf1976ff47c397948c83b0b9d294d
DDFrank <403216554@qq.com>
2018-12-24 21:22:53 +0800

    upd

--- src/main/java/com/frank/testData/MainVerticle.java
+++ src/main/java/com/frank/testData/MainVerticle.java
@@ -1,5 +1,6 @@
 package com.frank.testData;
 
+import com.frank.testData.dao.DBVerticle;
 import com.frank.testData.endpoint.web.HttpVerticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
@@ -7,12 +8,28 @@ import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private HttpVerticle httpVerticle;
-
   @Override
   public void start(Future<Void> startFuture) throws Exception {
+    Future<String> dbFuture = Future.future();
+    // 县初始化DB，再初始化HTTP服务
+    vertx.deployVerticle(new DBVerticle(), dbFuture.completer());
+    dbFuture.compose(id -> {
+        Future<String> httpFuture = Future.future();
 
-    httpVerticle.start();
+        vertx.deployVerticle(new HttpVerticle(), httpFuture.completer());
+        // 返回一个future接着解析
+        return httpFuture;
+    }).setHandler(ar -> {
+        if (ar.succeeded()) {
+            // 完成整个启动
+            System.out.println("");
+            startFuture.complete();
+        } else {
+            // 启动失败
+            startFuture.fail(ar.cause());
+        }
+    });
+
   }
 
 }
--- src/main/java/com/frank/testData/dao/DBVerticle.java
+++ src/main/java/com/frank/testData/dao/DBVerticle.java
@@ -1,8 +1,22 @@
 package com.frank.testData.dao;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.jooq.impl.DataSourceConnectionProvider;
+
 /**
  * .Description: Author: 金君良 Date: 2018/12/22 0022 上午 11:33
  */
-public class DBVerticle {
+public class DBVerticle extends AbstractVerticle {
+
+    private DSLContext dslContext;
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        //dslContext = DSL.using()
+
+    }
 
 }
